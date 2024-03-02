527dbdcea50762857c2641a94dc2e5988e9a42c5
Niespodzianek <michal_grzyb@yahoo.pl>
2024-03-02 20:17:06 +0100

    Ruch gracza jako metoda klasy.

--- gra/main.py
+++ gra/main.py
@@ -1,34 +1,36 @@
 from mapa import mapa_gry as mapa_gry
 class Gracz:
-    def __init__(self, pozycja, zdrowie=0, sila=0):
+    def __init__(self, pozycja, zdrowie=10, sila=10):
         self.imie = "Bezimienny"
         self.pozycja = pozycja
         self.zdrowie = zdrowie
         self.sila = sila
 
+    def ruch_gracza(self):
+        print("Ruch gracza")
+        ruch = True
+        while ruch:
+            kierunek = input("W którym kierunku chcesz się poruszyć: góra, prawo, dół, lewo\n> ")
+            if kierunek not in ["góra", "dół", "prawo", "lewo"]:
+                print("Zły wybór. Jeszcze raz.")
+            else:
+                if self.pozycja[kierunek] == 0:
+                    print("Koniec mapy. Wybierz jeszcze raz inny kierunek.")
+                else:
+                    self.pozycja = mapa_gry[self.pozycja[kierunek]]
+                    self.sila = self.sila - 1
+                    ruch = False
+
     def prezentacja(self):
-        print(f"Gracz {self.imie} znajduje się w {self.pozycja['nazwa']}, ma siłę {self.sila} i zdrowie {self.zdrowie}")
+        print(f"Gracz {self.imie} znajduje się na pozycji {self.pozycja['nazwa']}, ma siłę {self.sila} i zdrowie {self.zdrowie}")
 
 # inicjalizacja gry
 zawodnik = Gracz(pozycja=mapa_gry[22], zdrowie=12, sila=20)
-def ruch_gracza():
-    ruch = True
-    while ruch:
-        zawodnik.prezentacja()
-        kierunek = input("W którym kierunku chcesz się poruszyć: góra, prawo, dół, lewo\n> ")
-        if kierunek not in ["góra", "dół", "prawo", "lewo"]:
-            print("Zły wybór ruchu. Jeszcze raz.")
-        else:
-            if zawodnik.pozycja[kierunek] == 0:
-                print("Koniec mapy. Wybierz jeszcze raz inny kierunek.")
-            else:
-                zawodnik.pozycja = mapa_gry[zawodnik.pozycja[kierunek]]
-                zawodnik.sila = zawodnik.sila - 1
-                ruch = False
-    zawodnik.prezentacja()
-
 def program():
-    ruch_gracza()
+    #ruch_gracza()
+    zawodnik.prezentacja()
+    zawodnik.ruch_gracza()
+    zawodnik.prezentacja()
 
 program()
 print("KONIEC PRACY PROGRAMU")
